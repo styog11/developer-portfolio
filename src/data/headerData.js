a3b38192f1bc7fd46334fc86abc9c0f623f121b5
styog11 <driss.eladama@usmba.ac.ma>
2024-10-04 17:31:35 +0100

    header fixed

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,7 +1,5 @@
-import resume from '../assets/pdf/resume.pdf'
-
 export const headerData = {
-    name: 'Driss Eladama',
+    name: 'drissEladama',
     title: "Mobile Developer",
     desciption:"Success isn’t luck—it’s the result of consistent effort. You’ll surpass others not by crawling, but by running while they crawl. The only fair comparison is between who you are today and who you were yesterday.",
     image: 'https://raw.githubusercontent.com/styog11/my_portfolio_assets/main/image.png',
